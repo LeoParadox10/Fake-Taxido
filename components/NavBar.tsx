5b1fb9fc0344e32b98b18ad8a7903fb0edcd8805
Smarak Patra <smarakpatra2711@gmail.com>
2024-06-09 02:56:09 +0530

    updated NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,21 @@
+"use client"
+
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 function NavBar() {
+
+    const router = useRouter();
+
+    const handleHomeClick = () => {
+        router.push('/sign-up');
+    };
+    const handleRoot = () => {
+        router.push('/')
+    }
+
     return (
         <div className='flex justify-between p-3 px-10 border-b-[1px] shadow-md'>
             <div className='flex gap-10 items-center'>
@@ -10,10 +23,15 @@ function NavBar() {
                     alt='logo'
                     width={80}
                     height={30}
-                    className='w-auto'
+                    className='w-auto cursor-pointer transition-all'
+                    onClick={handleRoot}
                 />
                 <div className='hidden md:flex gap-6'>
-                    <h2 className='hover:bg-gray-200 p-2 rounded-md cursor-pointer transition-all'>Home</h2>
+                    <button onClick={handleHomeClick}>
+                        <h2 className='hover:bg-gray-200 p-2 rounded-md cursor-pointer transition-all'>
+                            Home
+                        </h2>
+                    </button>
                     <h2 className='hover:bg-gray-200 p-2 rounded-md cursor-pointer transition-all'>History</h2>
                     <h2 className='hover:bg-gray-200 p-2 rounded-md cursor-pointer transition-all'>Help</h2>
                 </div>
